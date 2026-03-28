255d2952be8a49c6de93791ea1161b4a6c4a797a
Lars Ludvigsen <larsll@outlook.com>
2026-03-28 13:40:26 +0100

    Remove code

--- deepracer_llm_agent/services/bedrock_service.py
+++
@@ -1,9 +0,0 @@
-"""
-This is a compatibility layer that re-exports the modular Bedrock implementation.
-Existing code can continue using this file as the entry point.
-"""
-
-from .bedrock import BedrockService
-
-# Export the BedrockService class as the default export
-__all__ = ['BedrockService']
